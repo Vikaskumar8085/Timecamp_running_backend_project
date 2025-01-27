7148e7c082c39b65ee0df9d430ffa6dcd8e22006
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2025-01-27 19:30:21 +0530

    update code

--- credential/credential.js
+++ credential/credential.js
@@ -5,6 +5,9 @@ const credentials = {
   SERVER_PORT: process.env.PORT || 6000,
   DB_URL: process.env.DB || "mongodb://127.0.0.1:27017/timecamp_db",
   JWT_SECRET: process.env.SECRET,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
 };
 
 module.exports = credentials;
