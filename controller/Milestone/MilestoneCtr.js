1f6c49298c046676d86b8a7712b3012265feaf89
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2025-02-06 22:50:12 +0530

    update

--- controller/Milestone/MilestoneCtr.js
+++ controller/Milestone/MilestoneCtr.js
@@ -5,17 +5,45 @@ const HttpStatusCodes = require("../../utils/StatusCodes/statusCodes");
 const milestoneCtr = {
   createmilestone: asyncHandler(async (req, res) => {
     try {
-      const response = await Milestone.insertMany(req.body);
-      if (!response) {
-        res.status(HttpStatusCodes.BAD_REQUEST);
+      const milestones = req.body; // Expecting an array of milestone objects
+
+      if (!Array.isArray(milestones) || milestones.length === 0) {
+        return res.status(400).json({
+          message: "Milestones data is required and should be an array.",
+        });
       }
-      return res.status(HttpStatusCodes.OK).json({
-        message: "add milestone successfully",
-        success: true,
-        result: response,
+
+      let insertedMilestones = [];
+
+      for (const item of milestones) {
+        const milestone = new Milestone({
+          Compnay_Id: "1",
+          Name: item.Name,
+          Description: item.Description,
+          Start_date: new Date(item.Start_date),
+          End_date: new Date(item.End_date),
+        });
+
+        const savedMilestone = await milestone.save();
+        insertedMilestones.push(savedMilestone);
+      }
+
+      res.json({
+        message: "Bulk upload successful!",
+        insertedCount: insertedMilestones.length,
       });
+
+      // console.log(insertedMilestones, "dasldkfskd");
+      //
+      // return res.status(HttpStatusCodes.OK).json({
+      //   message: "add milestone successfully",
+      //   success: true,
+      //   result: insertedMilestones,
+      // });
     } catch (error) {
       throw new Error(error?.message);
     }
   }),
 };
+
+module.exports = milestoneCtr;
