7de58a6189eef6dbaa84660e3f7f9e0805a96fdd
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2025-05-22 22:51:55 +0530

    update code

--- controller/Authcontrollers/Forecast/forecastingCtr.js
+++ controller/Authcontrollers/Forecast/forecastingCtr.js
@@ -4,6 +4,7 @@ const HttpStatusCodes = require("../../../utils/StatusCodes/statusCodes");
 const Company = require("../../../models/Othermodels/Companymodels/Company");
 const moment = require("moment");
 const StaffMember = require("../../../models/AuthModels/StaffMembers/StaffMembers");
+const RoleResource = require("../../../models/Othermodels/Projectmodels/RoleResources");
 
 const forecastingCtr = {
   TeamforecastReports: asynchandler(async (req, res) => {
@@ -13,7 +14,7 @@ const forecastingCtr = {
         res.status(HttpStatusCodes.UNAUTHORIZED);
         throw new Error("UnAuthorized user please login");
       }
-      const checkcompany = await Company.findOne({UserId: user.user_Id});
+      const checkcompany = await Company.findOne({ UserId: user.user_Id });
       if (checkcompany) {
         res.status(HttpStatusCodes.NOT_FOUND);
         throw new Error("Company Not found");
@@ -27,17 +28,75 @@ const forecastingCtr = {
       const minJoinDate = today.clone().subtract(maxExp, "years").toDate();
 
       const response = await StaffMember.find({
-        Joining_Date: {$gte: minJoinDate, $lte: maxJoinDate},
-        DesignationId: {$in: 9},
+        Joining_Date: { $gte: minJoinDate, $lte: maxJoinDate },
+        DesignationId: { $in: 2 },
       });
 
-      if (!response || response.length === 0) {
-        res.status(HttpStatusCodes.NOT_FOUND);
-        throw new Error(
-          "No staff members found with the specified experience range"
-        );
-      }
+      const fetchfilterprojects = await Promise.all(
+        response.map(async (item) => {
+          const resp = await RoleResource.find({
+            RRId: { $in: response.map((emp) => emp.staff_Id) },
+            RId: { $in: 2 },
+          });
 
+          const projectids = await resp.map((ids) => ids.ProjectId);
+
+          const findProject = await Project.find({
+            ProjectId: { $in: projectids },
+          });
+          const today = moment().startOf("day");
+
+          const activeProjects = findProject.filter((project) => {
+            const projectEndDate = moment(project?.endDate).startOf("day");
+            return projectEndDate.isSameOrAfter(today);
+          });
+
+          // Step 2: Extract active Project IDs
+          const projectIds = activeProjects.map((project) => project.ProjectId);
+
+          // Step 3: Fetch RoleResource entries for those projects
+          const roleResources = await RoleResource.find({
+            ProjectId: { $in: projectIds },
+          });
+
+          // Step 4: Group by RRid and sum Engagement_Ratio
+          const engagementMap = {};
+
+          roleResources.forEach((rr) => {
+            const rrid = rr.RRid;
+            if (!engagementMap[rrid]) {
+              engagementMap[rrid] = 0;
+            }
+            engagementMap[rrid] += rr.Engagement_Ratio;
+          });
+
+          // Step 5: Filter RRid where total ratio < 100
+          const availableRRids = Object.entries(engagementMap)
+            .filter(([_, totalRatio]) => totalRatio < 100)
+            .map(([rrid]) => rrid);
+
+          // ✅ Result
+          console.log(availableRRids);
+
+          const fetchstaff = await StaffMember.find({
+            staff_Id: { $in: availableRRids },
+          });
+          return fetchstaff;
+        })
+      );
+
+      // const projectResponse = await Project.find({
+      //   ProjectId: { $in: resp.map((emp) => emp.ProjectId) },
+      // });
+
+      // if()
+      // const projectlength = projectResponse.length;
+      // if (!response || response.length === 0) {
+      //   res.status(HttpStatusCodes.NOT_FOUND);
+      //   throw new Error(
+      //     "No staff members found with the specified experience range"
+      //   );
+      // }
 
       // const findproject = await Project.find({})
 
@@ -60,7 +119,7 @@ const forecastingCtr = {
       return res.status(HttpStatusCodes.OK).json({
         success: true,
         message: "Forecasting report",
-        data: response,
+        data: fetchfilterprojects,
       });
     } catch (error) {
       throw new Error(error?.message);
@@ -73,7 +132,7 @@ const forecastingCtr = {
         res.status(HttpStatusCodes.UNAUTHORIZED);
         throw new Error("UnAuthorized user please login");
       }
-      const checkcompany = await Company.findOne({UserId: user.user_Id});
+      const checkcompany = await Company.findOne({ UserId: user.user_Id });
       if (checkcompany) {
         res.status(HttpStatusCodes.NOT_FOUND);
         throw new Error("Company Not found");
